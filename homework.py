47a42c5b2261f69f328b032f0cced637780e3405
Константин <ikonstantin1991@mail.ru>
2021-04-19 20:40:15 +0300

    Some exceptions have been fixed

--- homework.py
+++ homework.py
@@ -1,11 +1,11 @@
 import logging
 import os
 import time
-# from logging.handlers import RotatingFileHandler
 
 import requests
 import telegram
 from dotenv import load_dotenv
+from requests.exceptions import RequestException
 
 load_dotenv()
 
@@ -21,10 +21,6 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 def parse_homework_status(homework):
     homework_name = homework.get("homework_name")
-    if not homework_name:
-        err_text = 'Свойства homework_name не существует'
-        logging.error(err_text)
-        return(err_text)
     statuses = {
         "rejected": 'К сожалению в работе нашлись ошибки.',
         "approved": ('Ревьюеру всё понравилось, можно '
@@ -32,13 +28,12 @@ def parse_homework_status(homework):
         "reviewing": 'Ревьюер приступил к проверке',
     }
     status = homework.get("status")
-    if not status:
-        err_text = 'Свойства status не существует'
+    if status is None or homework_name is None:
+        err_text = 'Свойства status или homework_name не существует'
         logging.error(err_text)
         return(err_text)
-    if status in statuses:
-        verdict = statuses[status]
-    else:
+    verdict = statuses.get(status)
+    if verdict is None:
         err_text = 'Неизвестный статус'
         logging.error(err_text)
         return err_text
@@ -53,11 +48,11 @@ def get_homework_statuses(current_timestamp):
             headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
         )
         return homework_statuses.json()
-    except Exception:
-        logging.exception()
+    except RequestException as error:
+        logging.exception(str(error) + '\nПроблемы с запросом')
         return {}
-    except ValueError:
-        logging.exception()
+    except ValueError as error:
+        logging.exception(error)
         return {}
 
 
@@ -78,7 +73,7 @@ def main():
                     bot
                 )
                 logging.info('Сообщение отправлено')
-            if new_homework.get("current_date"):
+            if new_homework.get("current_date") is not None:
                 current_timestamp = new_homework.get("current_date")
             else:
                 current_timestamp = int(time.time())
